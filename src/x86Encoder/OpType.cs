b12d42b1be2393b0582fbc6ba83e06218b185f50
Robert Sundström <robert.sundstrom@outlook.com>
2016-09-04 18:55:55 +0200

    Added op codes and tests.

--- src/x86Encoder/OpType.cs
+++ src/x86Encoder/OpType.cs
@@ -68,6 +68,40 @@
             return bytes;
         }
 
+        #region Op Types
+
+        public static readonly OpType Push = new OpType(nameof(Push), new byte[] { 0x6A }, true, true, true, true);
+
+        public static readonly OpType Pop = new OpType(nameof(Pop), new byte[] { 0x8F }, true, true, true, true);
+
         public static readonly OpType Add = new OpType(nameof(Add), new byte[]{0x00}, true, true, true, true);
+
+        public static readonly OpType Sub = new OpType(nameof(Sub), new byte[] { 0x28 }, true, true, true, true);
+
+        public static readonly OpType IMul = new OpType(nameof(IMul), new byte[] { 0x6B }, true, true, true, true);
+
+        public static readonly OpType Div = new OpType(nameof(Div), new byte[] { 0xF6 }, true, true, true, true);
+
+        public static readonly OpType Inc = new OpType(nameof(Inc), new byte[] { 0xFE }, true, true, true, true);
+
+        public static readonly OpType Dec = new OpType(nameof(Dec), new byte[] { 0xFF }, true, true, true, true);
+
+        public static readonly OpType Mov = new OpType(nameof(Mov), new byte[] { 0x88 }, true, true, true, true);
+
+        public static readonly OpType Ret = new OpType(nameof(Ret), new byte[] { 0xC2 }, true, true, true, true);
+
+        public static readonly OpType IRet = new OpType(nameof(IRet), new byte[] { 0xCF }, true, true, true, true);
+
+        public static readonly OpType Call = new OpType(nameof(Call), new byte[] { 0xE8 }, true, true, true, true);
+
+        public static readonly OpType Jmp = new OpType(nameof(Jmp), new byte[] { 0xE9 }, true, false, false, true);
+
+        public static readonly OpType Jz = new OpType(nameof(Jz), new byte[] { 0x74 }, true, false, false, true);
+
+        public static readonly OpType Jnz = new OpType(nameof(Jnz), new byte[] { 0x75 }, true, false, false, true);
+
+        public static readonly OpType Cmp = new OpType(nameof(Cmp), new byte[] { 0x38 }, true, true, true, false);
+
+        #endregion
     }
 }
---
+++ test/x86Encoder.Tests/OpCodes/MovOpCodeTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace x86Encoder.Tests.OpCodes
+{
+    /// <summary>
+    /// http://x86.renejeschke.de/html/file_module_x86_id_176.html
+    /// </summary>
+    public class MovOpCodeTest
+    {
+        [Fact(DisplayName = nameof(Mov))]
+        public void Mov()
+        {
+            byte expectedOpByte = 0x88;
+
+            var opType = OpType.Mov;
+
+            var opByte = opType.GetBytes().Single();
+            Assert.Equal(expectedOpByte, opByte);
+        }
+
+        [Fact(DisplayName = nameof(Mov_Eb_Gb))]
+        public void Mov_Eb_Gb()
+        {
+            byte expectedOpByte = 0x88;
+
+            var opCode = OpCode.Create(OpType.Mov, OpCodeDirection.RegToRM);
+
+            var opByte = opCode.GetBytes().Single();
+            Assert.Equal(expectedOpByte, opByte);
+        }
+
+        [Fact(DisplayName = nameof(Mov_Ev_Gv))]
+        public void Mov_Ev_Gv()
+        {
+            byte expectedOpByte = 0x89;
+
+            var opCode = OpCode.Create(OpType.Mov, OpCodeDirection.RegToRM, true);
+
+            var opByte = opCode.GetBytes().Single();
+            Assert.Equal(expectedOpByte, opByte);
+        }
+
+        [Fact(DisplayName = nameof(Mov_Gb_Eb))]
+        public void Mov_Gb_Eb()
+        {
+            byte expectedOpByte = 0x8A;
+
+            var opCode = OpCode.Create(OpType.Mov, OpCodeDirection.RMToReg);
+
+            var opByte = opCode.GetBytes().Single();
+            Assert.Equal(expectedOpByte, opByte);
+        }
+
+        [Fact(DisplayName = nameof(Mov_Gv_Ev))]
+        public void Mov_Gv_Ev()
+        {
+            byte expectedOpByte = 0x8B;
+
+            var opCode = OpCode.Create(OpType.Mov, OpCodeDirection.RMToReg, true);
+
+            var opByte = opCode.GetBytes().Single();
+            Assert.Equal(expectedOpByte, opByte);
+        }
+
+        [Fact(DisplayName = nameof(Mov_Gw_Ew), Skip = "Why is this wrong?")]
+        public void Mov_Gw_Ew()
+        {
+            byte expectedOpByte = 0x8C;
+
+            var opCode = OpCode.Create(OpType.Mov, OpCodeDirection.RMToReg, true);
+
+            var opByte = opCode.GetBytes().Single();
+            Assert.Equal(expectedOpByte, opByte);
+        }
+    }
+}
